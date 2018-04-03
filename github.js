66ac843258bf6d84a16d4b7f6f3cb9f5276e9ca2
Paul Carrillo <paulcarrillo05@gmail.com>
2018-04-03 00:42:15 -0600

    Application complete displaying repos and github user data by account name

---
+++ github.js
@@ -0,0 +1,23 @@
+class Github {
+  constructor() {
+    this.client_id = // User create a id from GitHub under personal account
+    this.client_secret = // User create a secret from GitHub under personal account
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
